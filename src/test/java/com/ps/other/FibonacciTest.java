e532fec0251de136d312e0eb7ec13d97329fc70d
arunponnaiah <arun.ponnaiah@gmail.com>
2013-11-17 17:10:08 -0500

    returning array

--- src/test/java/com/ps/other/FibonacciTest.java
+++ src/test/java/com/ps/other/FibonacciTest.java
@@ -1,6 +1,7 @@
 package com.ps.other;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,7 +18,9 @@ public class FibonacciTest {
 
 	@Test
 	public void testGenerate() {
-		this.fibonacci.generate(10);
+		int[] expecteds ={0,1,1,2,3,5,8,13,21,34};
+		int[] actuals = this.fibonacci.generate(10);
+		Assert.assertArrayEquals(expecteds, actuals);
 	}
 
 }
