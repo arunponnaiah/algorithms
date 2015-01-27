0219f7d7ef1295fdb7311b0f84668cf6c0c0f62a
Arun Ponnaiah <aponnaiah@lendingclub.com>
2015-01-27 12:12:05 -0800

    added findBySorting / findByBruteForce methods and its TC's

--- src/main/java/com/ps/java2novice/DuplicateNumber.java
+++ src/main/java/com/ps/java2novice/DuplicateNumber.java
@@ -1,17 +1,36 @@
 package com.ps.java2novice;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class DuplicateNumber {
 	
-	public int find(int[] inputArr){
-		int inputArrLen = inputArr.length;
-		
-		int input =0;
-		for(int i : inputArr){
-			input = input+i;
+	// O(n) time complexity
+		public Set<Integer> findBySorting(int[] inputArr){
+			Set<Integer> set = new HashSet<Integer>();
+			
+			Arrays.sort(inputArr);
+			for(int i=1;i<inputArr.length;i++){
+				int current = inputArr[i];
+				int previousIndex = (i-1);
+				int previous = inputArr[previousIndex];
+				if(current == previous)
+					set.add(current);
+			}
+			return set;
 		}
-		
-		System.out.println( "input >>>>> " + input);
-		return 0;
-	}
+		// O(n^2) time complexity
+		public Set<Integer> findByBruteForce(int[] inputArr){
+			Set<Integer> set = new HashSet<Integer>();
+			for(int i=0;i<inputArr.length;i++){
+				for(int j=i+1;j<inputArr.length;j++){
+					if(inputArr[i]==inputArr[j]){
+						set.add(inputArr[i]);
+					}
+				}	
+			}
+			return set;
+		}
 
 }
--- src/test/java/com/ps/java2novice/DuplicateNumberTest.java
+++ src/test/java/com/ps/java2novice/DuplicateNumberTest.java
@@ -1,22 +1,36 @@
 package com.ps.java2novice;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class DuplicateNumberTest {
 	private DuplicateNumber duplicateNumber;
+	private int[] inputArr;
 	
 	@Before
 	public void setup(){
-		duplicateNumber = new DuplicateNumber();
+		this.duplicateNumber = new DuplicateNumber();
+		this.inputArr = new int[]{100,1,2,3,2,4,5,5,100};
 	}
 
 	@Test
-	public void test() {
-		int[] inputArr = new int[]{1,2,3,4,5};
-		System.out.println(this.duplicateNumber.find(inputArr));
+	public void testFindBySorting() {
+		Set<Integer> actuals = this.duplicateNumber.findBySorting(inputArr);
+		Set<Integer> expecteds = new HashSet<Integer>(Arrays.asList(100,2,5));
+		assertEquals(expecteds, actuals);
+	}
+	
+	@Test
+	public void findByBruteForce(){
+		Set<Integer> actuals = this.duplicateNumber.findByBruteForce(inputArr);
+		Set<Integer> expecteds = new HashSet<Integer>(Arrays.asList(100,2,5));
+		assertEquals(expecteds, actuals);
 	}
 
 }
