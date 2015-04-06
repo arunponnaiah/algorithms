62ed0c3f5708ab7d831b9a9aeb9e4bf46b7da0f9
Arun Ponnaiah <aponnaiah@lendingclub.com>
2015-04-06 09:40:06 -0700

    updated algorithm to find common elements btwn 2 arrays

--- src/main/java/com/algorithms/java2novice/CommonElementsInArray.java
+++ src/main/java/com/algorithms/java2novice/CommonElementsInArray.java
@@ -1,25 +1,41 @@
 package com.algorithms.java2novice;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class CommonElementsInArray {
 	private int[] firstArray;
 	private int[] secondArray;
+	private Map<Integer, Boolean> map;
 
 	public CommonElementsInArray(int[] firstArray, int[] secondArray) {
 		this.firstArray = firstArray;
 		this.secondArray = secondArray;
+		this.map = new HashMap<Integer, Boolean>();
 	}
 
 	/**
 	 * Find common elements between 2 arrays
 	 * @return
 	 */
-	public int[] findCommonElements() {
-		if(firstArray.length > secondArray.length){
-			// sort firstArray
-		}else{
-			// sort secondArray
+	public Set<Integer> findCommonElements() {
+		Set<Integer> commonElements = new HashSet<Integer>();
+		init(firstArray);
+		for(int i : secondArray){
+			Boolean element = map.get(i);
+			if(element != null && element.equals(Boolean.TRUE)){
+				commonElements.add(i);
+			}
 		}
-		return null;
+		return commonElements;
+	}
+
+	private void init(int[] arr) {
+		for(int i : arr){
+			this.map.put(i, true);
+		}
 	}
 	
 	
--- src/test/java/com/algorithms/java2novice/CommonElementsInArrayTest.java
+++ src/test/java/com/algorithms/java2novice/CommonElementsInArrayTest.java
@@ -1,6 +1,9 @@
 package com.algorithms.java2novice;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,15 +16,14 @@ public class CommonElementsInArrayTest {
 	
 	@Before
 	public void setUp() throws Exception {
-		this.firstArray= new int[]{1,2,3};
-		this.secondArray= new int[]{5,4,3};
+		this.firstArray= new int[]{1,2,3,10,20};
+		this.secondArray= new int[]{5,4,3,10};
 		this.commonElementsInArray = new CommonElementsInArray(firstArray,secondArray);
 	}
 
 	@Test
 	public void testFind() {
-		assertArrayEquals(new int[]{5}, this.commonElementsInArray.findCommonElements());
-		
+		Assert.assertEquals(new HashSet<Integer>(Arrays.asList(3,10)), this.commonElementsInArray.findCommonElements());
 	}
 
 }
