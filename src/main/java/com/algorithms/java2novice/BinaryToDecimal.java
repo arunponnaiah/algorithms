2827d65657bc00286aeaed4c85ef168817474e7b
Arun Ponnaiah <aponnaiah@lendingclub.com>
2015-04-09 09:49:43 -0700

    added java docs in  binary to decimal conversion algorithm

--- src/main/java/com/algorithms/java2novice/BinaryToDecimal.java
+++ src/main/java/com/algorithms/java2novice/BinaryToDecimal.java
@@ -1,8 +1,19 @@
 package com.algorithms.java2novice;
 
+/**
+ * convert binary to decimal number
+ * @author aponnaiah
+ *
+ */
 public class BinaryToDecimal {
 	private int exp=0;
 	private int decimal =0;
+	
+	/**
+	 * convert binary to decimal number using recursion
+	 * @param binary
+	 * @return
+	 */
 	public int convert(int binary){
 		if(binary!=0){
 			decimal += (binary%10) * Math.pow(2, exp);
