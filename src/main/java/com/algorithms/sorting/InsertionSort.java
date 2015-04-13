292875bf31f101cf50c8386bead6bf1635e1f025
arun <arun.ponnaiah@gmail.com>
2015-04-13 02:23:46 -0700

    Added insertion sorting algorithm

---
+++ src/main/java/com/algorithms/sorting/InsertionSort.java
@@ -0,0 +1,36 @@
+package com.algorithms.sorting;
+
+public class InsertionSort {
+	/**
+	 * Insertion Sorting using (while loop) subroutine 
+	 * @param arr
+	 * @return
+	 */
+	public int[] sort(int[] arr){
+		for(int i=1; i<arr.length;i++){
+			int currentIndex=i;
+			int previousIndex=currentIndex-1;
+			while( previousIndex >=0
+					&& arr[previousIndex] > arr[currentIndex]){
+				this.swap(arr, currentIndex, previousIndex);;
+				--currentIndex;
+				--previousIndex;
+			}
+		}
+		
+		return arr;
+	}
+
+	/**
+	 * Swaps items with no temp variable
+	 * @param arr
+	 * @param currentIndex
+	 * @param previousIndex
+	 */
+	private void swap(int[] arr , int currentIndex, int previousIndex) {
+		arr[currentIndex] = arr[currentIndex] + arr[previousIndex];
+		arr[previousIndex]  = arr[currentIndex] - arr[previousIndex];
+		arr[currentIndex] = arr[currentIndex] - arr[previousIndex];
+	}
+
+}
---
+++ src/test/java/com/algorithms/sorting/InsertionSortTest.java
@@ -0,0 +1,20 @@
+package com.algorithms.sorting;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InsertionSortTest {
+	private InsertionSort insertionSort;
+	@Before
+	public void setUp() throws Exception {
+		this.insertionSort = new InsertionSort();
+	}
+
+	@Test
+	public void testSort() {
+		assertArrayEquals(new int[]{0,1,3,4,5},this.insertionSort.sort(new int[]{5,4,3,0,1}));
+	}
+
+}
