a6d87702c6f3d6f1a82880b435b8a11e84c2bbf2
Arun Ponnaiah <arun.ponnaiah@gmail.com>
2016-10-18 12:01:23 -0700

    refactored code

--- src/main/java/com/algorithms/list/MinimumDiffInList.java
+++ src/main/java/com/algorithms/list/MinimumDiffInList.java
@@ -3,13 +3,25 @@ package com.algorithms.list;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Find minimum difference between elements with in list.
+ * @author arunponnaiah
+ *
+ */
 public class MinimumDiffInList {
+	
+	/**
+	 * Calculate minimum difference using sorting.
+	 * @param input
+	 * @return int
+	 */
 	public int get(ArrayList<Integer> input) {
 		Collections.sort(input);
 		int minDiff = input.get(1) - input.get(0);
 		
-		for(int i=2;i< input.size(); i++){
-			minDiff = Math.min(minDiff, input.get(i) - input.get(i-1));
+		for(int i=2;i< input.size()-1; i++){
+			int diff = input.get(i+1) - input.get(i);
+			minDiff = Math.min(minDiff, diff);
 		}
 		return minDiff;
 	}
