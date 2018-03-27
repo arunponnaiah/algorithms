cbaaa9195c210b57f0560bead8be855c50b9ec2e
Arun Ponnaiah <aponnai@arunponnaiah.americas.gap.com>
2018-03-26 22:35:49 -0700

    removed binary tree node creation steps in unit test

--- src/test/java/com/algorithms/ds/BinaryTreeTest.java
+++ src/test/java/com/algorithms/ds/BinaryTreeTest.java
@@ -60,16 +60,7 @@ public class BinaryTreeTest {
     @Test
     public void testConstructBinaryTreeForInorder(){
         List<Integer> list = new LinkedList<Integer>(Arrays.asList(1,2,3,4,5));
-        this.binaryTree = new BinaryTree();
-        rootNode = this.binaryTree.new Node(4);
-        rootNode.leftNode = this.binaryTree.new Node(2);
-        rootNode.leftNode.leftNode = this.binaryTree.new Node(1);
-        rootNode.leftNode.rightNode = this.binaryTree.new Node(3);
-        rootNode.rightNode = this.binaryTree.new Node(5);
-
         BinaryTree.Node root = this.binaryTree.constructBinaryTreeFromInorderTraversal(list,0, list.size()-1, null);
         Assert.assertEquals(list, this.binaryTree.inOrderTraversal(root));
     }
-
-
 }
